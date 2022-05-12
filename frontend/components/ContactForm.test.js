6edb332f0d37792779149d535e98a74dea039afd
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-05-12 16:53:20 -0500

    through third test

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -14,13 +14,18 @@ test('renders the contact form header', () => {
     const header = screen.queryByText(/contact form/i);
     expect(header).toBeInTheDocument;
     expect(header).toBeTruthy;
-    expect(header).toHaveTextContent(/contact form/i)
+    expect(header).toHaveTextContent(/contact form/i);
 
 });
 
-// test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+    render(<ContactForm />)
+    const firstNameField = screen.getByLabelText(/first name*/i);
+    userEvent.type(firstNameField, "adam")
 
-// });
+    const errorMessages = await screen.findAllByTestId("error")
+    expect(errorMessages).toHaveLength(1);
+});
 
 // test('renders THREE error messages if user enters no values into any fields.', async () => {
 
